8a4d24e5418da6cd040db86f5920388bd88aa0e0
Antonio Peña Peña <antoniopenapena52@gmail.com>
2025-07-30 19:42:12 +0100

    Astro RPC 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export { Obs } from "./obs";
-export { Get } from "./cache";
-export { GetRxController } from "./controller";
-export { useGet, useOnObsChange } from "./hooks";
-export type { GetRxControllerFactory } from "./types";
+export { createARPC } from './server';
+export { createARPCClient } from './client';
