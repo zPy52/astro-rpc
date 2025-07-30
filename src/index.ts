d29e20ea58f004ee77eb5736b0de1d22ff7a9c09
Antonio Peña Peña <antoniopenapena52@gmail.com>
2025-07-30 22:22:02 +0100

    1.0.5

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { useEndpoint } from "@/hook";
 import { EndpointInfo } from "./info";
 import { createEndpoint } from "@/server";
 
-export const arpc = {
+export const a = {
   use: useEndpoint,
   info: EndpointInfo,
   create: createEndpoint,
 };
+
+export const arpc = a;
